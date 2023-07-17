35f8060f225de3bbc3594e23e85646230ff2e72c
Halie Chmura <halie@sightsource.net>
2023-07-17 15:16:24 -0400

    save

---
+++ index.js
@@ -0,0 +1,60 @@
+const _ = require('lodash');
+
+console.log("successfully running")
+
+
+const s2Employees = [
+  {
+    firstName: "Dane",
+    lastName: "Cook",
+    favoriteColor: "red",
+    favoriteNumbers: [1, 12, 5]
+  },
+  {
+    firstName: "Claudia",
+    lastName: "Tahbaz",
+    favoriteColor: "purple",
+  },
+  {
+    firstName: "Jesse",
+    favoriteColor: "blue",
+    favoriteNumbers: []
+  },
+  {
+    firstName: "Chris",
+    lastName: "Jackson",
+    favoriteColor: "blue",
+    favoriteNumbers: [100]
+  },
+  {
+    firstName: "James",
+    favoriteColor: "pink",
+    favoriteNumbers: [100, 50, 25]
+  },
+  {
+    firstName: "David",
+    favoriteNumbers: [780, 20]
+  }
+]
+
+
+/*
+1. 
+Get a list of all the S2 employees favorite colors without any duplicates
+ie result should contain a list that resembles the following: ["red", "yellow", "blue"] (that's not the answer though)
+*/
+
+
+
+/*
+2. Return the same object s2Employees but for each person, have a new field called 'sumOfFavoriteNumbers' that is the sum of each person's fav numbers
+*/
+
+
+/*
+3. Get all the S2 employees whose favorite color is blue or purple and order the results by firstName
+*/
+
+
+
+
